631d19d7a58517a886068efeb032d9760593dee2
Markus Luan <markus23_luan@hotmail.com>
2022-04-23 15:51:25 -0300

    Melhorando sprite inimigo

--- js/cenario.js
+++ js/cenario.js
@@ -1,6 +1,6 @@
 let posCenario = 0;
 let velocidadeCenario = 2;
-const velocidadeCenarioMaxima = 40;
+const velocidadeCenarioMaxima = 30;
 
 function desenharRua(){
     canvas_obj.fillStyle = "#555";
